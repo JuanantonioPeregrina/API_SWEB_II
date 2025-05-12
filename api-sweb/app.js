44561ffd172a7fe7841b65d76539862e31106deb
Crramos <carlos.ramosrial@usp.ceu.es>
2025-05-13 01:13:12 +0200

    mmmm, va pero no va

--- api-sweb/app.js
+++ api-sweb/app.js
@@ -3,6 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load(path.join(__dirname, '../openapi.yml'));
+
 
 var indexRouter = require('./routes/index');
 const consolasRouter = require('./routes/consolas');
@@ -25,6 +29,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/', consolasRouter);
 app.use('/', rawgRoutes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
